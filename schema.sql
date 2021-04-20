3f4c6596d320a429420ddd94a81088308926a040
Sajeda-Araidah <sajedaaraidah@gmail.com>
2021-04-20 13:09:51 +0300

    final exam

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS quote (
+quote_id SERIAL PRIMARY KEY ,
+quote VARCHAR(200),
+character VARCHAR(200),
+ img VARCHAR(200),
+ characterDirection VARCHAR(200)
+
+);
--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const PORT = process.env.PORT || 3000;
 
 // Express middleware
 // Utilize ExpressJS functionality to parse the body of the request
-
+app.use(express.urlencoded({extended:true}));
 // Specify a directory for static resources
-
+app.use(express.static('./public/'))
 // define our method-override reference
-
+app.use(methodOverride('_method'));
 // Set the view engine for server-side templating
+app.set('view engine','ejs');
 
 // Use app cors
 
@@ -30,7 +31,12 @@ const client = new pg.Client(process.env.DATABASE_URL);
 
 // app routes here
 // -- WRITE YOUR ROUTES HERE --
-
+app.get('/',indexHandler);
+app.post('/'.addFavHandler);
+app.get('/data',myDataHandler);
+app.get('detials/:quote_id',detailsHandler);
+app.put('detials/:quote_id',detailsUpdateHandler);
+app.delet('detials/:quote_id',detailsDeleteHandler);
 
 // callback functions
 // -- WRITE YOUR CALLBACK FUNCTIONS FOR THE ROUTES HERE --
@@ -41,3 +47,64 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect().then(() =>
     app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 );
+
+function indexHandler(request ,response){
+ let url ='https://thesimpsonsquoteapi.glitch.me/quotes?count=10';
+ superagent.get(url).set('User-Agent', '1.0').then(x =>{
+     let data = x.body ;
+    response.render('index' ,{ result:data})
+ })
+}
+
+////////////////////////////////////////////////////
+function addFavHandler(request,response){
+    let quote=request.body.quote;
+    let character =request.body.character;
+    let img =request.body.img ;
+    let characterDirection=request.body.characterDirection;
+    let values =[quote,character,img, characterDirection];
+    let SQL = `INSERT INTO quote (quote,character,img, characterDirection) VALUES ($1,$2,$3 ,$4) RETURNING *`
+    client.query(SQL,values).then (y =>{
+        response.redirect('/')
+    })
+}
+////////////////////////////////////////////////////
+function myDataHandler(request,response){
+    let SQL =`SELECT * FROM quote `
+    client.query(SQL).then (y =>{
+        response.redirect('mydata', {collection :y.rows})
+    })
+}
+////////////////////////////////////////////////////
+ function detailsHandler(request,response){ 
+let quote_id =request.params.quote_id;
+    let SQL =`SELECT * FROM quote WHERE quote_id =$1 `
+    client.query(SQL ,[quote_id]).then (y =>{
+        response.redirect(`/details/${quote_id}`)
+    })
+ }
+/////////////////////////////////////////////////////
+function detailsUpdateHandler(request,response){
+    let quote=request.body.quote;
+    let character =request.body.character;
+    let img =request.body.img ;
+    let characterDirection=request.body.characterDirection;
+    let quote_id=request.body.quote_id;
+    let values=[quote,character,img,characterDirection,quote_id];
+    let SQL =`UPDATE quote (quote,character,img,characterDirection) SET (quote =$2,character=$2,img=$3,characterDirection=$4
+   WHERE quote_id =$6`
+   client.query(SQL ,values).then (y =>{
+    response.redirect(`/details/${quote_id}`)
+})
+
+}
+////////////////////////////////////////////////////
+function detailsDeleteHandler(request,response){
+    let quote_id=request.body.quote_id;
+    let SQL=`DELETE FROM quote WHERE quote_id =$1`
+    client.query(SQL).then (y =>{
+        response.redirect('/')
+    })
+
+}
+
